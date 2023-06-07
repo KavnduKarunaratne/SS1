d4d5940d43dd661542029eca13e87c3d47847fc1
Kavindu <cb010103@students.apiit.lk>
2023-06-07 20:23:25 +0530

    code

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<x-app-layout>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Home</title>
+    </head>
+    <body class="bg-gray-100">
+        <div class="relative">
+            <img src="{{URL('images/Main.jpg')}}" alt="" class="px-8">
+            <div class="absolute top-1/4 text-white p-9 w-1/2 m-14">
+                <h2 class="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores unde similique voluptates minus maiores doloribus ad at deleniti? Dicta aliquam fugiat quibusdam accusamus corporis, illo asperiores eveniet quos ipsum quaerat!</h2>
+                <button class="bg-white text-black p-2 rounded">SHOP NOW</button>
+            </div>
+        </div>
+        <div class="w-">
+
+        </div>
+    </body>
+</x-app-layout>
+</html>
